64637d0f09d5816e49e19d453afa6cd555889f36
suchasplus <suchasplus@gmail.com>
2017-03-13 07:49:09 +0800

    set for week 3 , none violate long in 32bit jvm

--- src/com/suchasplus/jconcurrent/Main.java
+++ src/com/suchasplus/jconcurrent/Main.java
@@ -4,41 +4,78 @@ package com.suchasplus.jconcurrent;
  * Powered by suchasplus@gmail.com on 2017/3/6.
  */
 public class Main {
-    public static void main(String[] args) throws InterruptedException {
 
-        //Q1
-        Thread t1 = new Thread(new SequenceOps(1));
-        Thread t2 = new Thread(new SequenceOps(2));
-        Thread t3 = new Thread(new SequenceOps(3));
-        ValueHolder.setStage(1);
+    final static String amd64 = "amd64";
 
-        t3.start();
-        t2.start();
-        t1.start();
+    public static void main(String[] args)  {
+        String arch = System.getProperty("os.arch");
+        if(arch.equals(amd64)) {
+            System.out.println("jvm is X86_64, exec on an i586 jvm, Now EXIT");
+            return;
+        }
+        LongHolder lh = new LongHolder();
+        One w1 = new One(lh);
+        NegOne w2 = new NegOne(lh);
+        w1.setDaemon(true);
+        w2.setDaemon(true);
+        w1.start();
+        w2.start();
+        while (true) {
+            long temp = lh.value;
+            String str = toBinary(temp);
+            if (!str.equals("0000000000000000000000000000000000000000000000000000000000000001")
+                    && !str.equals("1111111111111111111111111111111111111111111111111111111111111111")) {
+                System.out.println("Long is not violate write on i586 arch");
+                System.out.println(temp);
+                System.out.println(str);
+                break;
+            }
+        }
 
-        t1.join();
-        t2.join();
-        t3.join();
+    }
 
-        System.out.println("Q1 All Exec-ed");
-
-        //Q2
-
-        Thread bt1 = new Thread(new QueueOps(true));
-        Thread bt2 = new Thread(new QueueOps(false));
-        Thread bt3 = new Thread(new QueueOps(true));
-        Thread bt4 = new Thread(new QueueOps(false));
+    private static String toBinary(long l) {
+        StringBuilder sb = new StringBuilder(Long.toBinaryString(l));
+        while (sb.length() < 64) {
+            sb.insert(0, "0");
+        }
+        return sb.toString();
+    }
 
 
-        bt4.start();
-        bt3.start();
-        bt2.start();
-        bt1.start();
 
-        bt1.join();
-        bt2.join();
-        bt3.join();
-        bt4.join();
+}
 
+class LongHolder {
+    public long value = 1L;
+}
+
+class One extends Thread {
+
+    public One(LongHolder lh) {
+        this.lh = lh;
+    }
+
+    private LongHolder lh;
+
+    public void run() {
+        while (true) {
+            lh.value = 1l;
+        }
+    }
+}
+
+class NegOne extends Thread {
+
+    public NegOne(LongHolder lh) {
+        this.lh = lh;
+    }
+
+    private LongHolder lh;
+
+    public void run() {
+        while (true) {
+            lh.value = -1l;
+        }
     }
 }
